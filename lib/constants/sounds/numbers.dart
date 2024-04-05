6bb7411506706a71f46726ac032556f1a3391a5e
kareem <01015143078kareem@gmail.com>
2024-04-05 00:37:45 -0700

    upload project

---
+++ lib/constants/sounds/numbers.dart
@@ -0,0 +1,5 @@
+class SoundsNumbers {
+  SoundsNumbers._();
+
+  static const String numberOne = 'assets/sounds/numbers/number_one_sound.mp3';
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:app_try2/constants/sounds/numbers.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PlayAudio());
+}
+
+class PlayAudio extends StatelessWidget {
+  const PlayAudio({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource(SoundsNumbers.numberOne),
+                    );
+                  };
+                },
+                child: const Icon(Icons.play_arrow),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
